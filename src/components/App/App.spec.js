80d5db64e7b03cf16496a848cef8c7aa91395a32
Alexandr Samylin <promter11@gmail.com>
2021-02-08 18:30:18 +0300

    Правки теста App

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -3,6 +3,8 @@ import { shallow } from "enzyme";
 
 import App from "./index";
 
+jest.useFakeTimers();
+
 describe("App component", () => {
   let wrapper;
 
@@ -12,5 +14,18 @@ describe("App component", () => {
 
   it("renders correctly", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("calls a function with setTimeout", (done) => {
+    jest.spyOn(React, "useEffect").mockImplementationOnce((f) => f());
+    jest.advanceTimersByTime(3000);
+
+    setTimeout(() => {
+      expect(wrapper.find("Preloader")).toBeFalsy();
+    }, 3000);
+
+    jest.useRealTimers();
+
+    done();
   });
 });
